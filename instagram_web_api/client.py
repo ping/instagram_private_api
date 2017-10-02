f3ee6f632ef07ac4a3a72f8d529285be42d427da
ping <ping@users.noreply.github.com>
2017-10-02 13:25:26 +0800

    Add license header

--- instagram_web_api/client.py
+++ instagram_web_api/client.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2017 https://github.com/ping
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 # -*- coding: utf-8 -*-
 
 import logging
