3888128f781a2fa4d9ca5228e1c667b6a0c5f80a
felixbreuer <hi@felixbreuer.me>
2019-07-29 12:08:23 +0200

    test again

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,9 +2,9 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 git add instapi/version.txt
-git commit -m "Bump Version (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+git commit -m "Bump Version (Build $TRAVIS_BUILD_NUMBER) [skip ci]"
 git remote rm origin
-git remote add origin https://breuerfelix:${GH_TOKEN}@github.com/breuerfelix/instapi.git > /dev/null 2>&1
+git remote add origin https://breuerfelix:${GH_TOKEN}@github.com/breuerfelix/instapi.git
 git push origin master
 
 cat instapi/version.txt | xargs git tag
