e6aa83222509ebfa4ed9b8d4622a39af13ff0565
felixbreuer <hi@felixbreuer.me>
2019-07-29 13:08:49 +0200

    test

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,7 +3,7 @@ git config --global user.name "Travis CI"
 
 git checkout master
 git add instapi/version.txt
-git commit -m "Bump Version (Build $TRAVIS_BUILD_NUMBER) [skip version]"
+git commit -m "Bump Version (Build $TRAVIS_BUILD_NUMBER) [skip version ci]"
 git remote rm origin
 git remote add origin https://breuerfelix:${GH_TOKEN}@github.com/breuerfelix/instapi.git
 git push --set-upstream origin master
