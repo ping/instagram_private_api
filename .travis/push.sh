be61c0e4c3f234d7f50ebb2563de7c3e4790a0e9
felixbreuer <hi@felixbreuer.me>
2019-07-29 12:23:24 +0200

    test again

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,7 +5,7 @@ git add instapi/version.txt
 git commit -m "Bump Version (Build $TRAVIS_BUILD_NUMBER) [skip ci]"
 git remote rm origin
 git remote add origin https://breuerfelix:${GH_TOKEN}@github.com/breuerfelix/instapi.git
-git push origin master
+git push HEAD:master
 
 cat instapi/version.txt | xargs git tag
 git push --tags
