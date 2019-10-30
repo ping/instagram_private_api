79c4d0f19ffb569919b1d210674884a22fd80c0b
ping <ping@users.noreply.github.com>
2019-10-30 15:31:27 +0800

    Fix up setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from os import path
+import io
 try:
     from setuptools import setup
 except ImportError:
@@ -19,18 +21,24 @@ packages = [
 ]
 test_reqs = [] if has_mock else ['mock']
 
+with io.open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='instagram_private_api',
     version=__version__,
     author='ping',
-    author_email='lastmodified@gmail.com>',
+    author_email='lastmodified@gmail.com',
     license='MIT',
     url='https://github.com/ping/instagram_private_api/tree/master',
     install_requires=[],
     test_requires=test_reqs,
     keywords='instagram private api',
     description='A client interface for the private Instagram API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=packages,
+    platforms=['any'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -38,5 +46,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
