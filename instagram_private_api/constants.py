0f1dcd76f845f33e437af5d06812f0636228c33d
Serdar Akarca <serdar@yuix.org>
2020-10-12 14:54:45 +0300

    change device name and android version

--- instagram_private_api/constants.py
+++ instagram_private_api/constants.py
@@ -3,25 +3,25 @@ class Constants(object):
     """Constants holder class that stores the bulk of the fixed strings used in the library."""
 
     IG_SIG_KEY = '19ce5f445dbfd9d29c59dc2a78c616a7fc090a8e018b9267bc4240a30244c53b'
-    IG_CAPABILITIES = '3brTvw=='
+    IG_CAPABILITIES = '3brTvwM='
     SIG_KEY_VERSION = '4'
-    APP_VERSION = '76.0.0.15.395'
+    APP_VERSION = '137.0.0.34.123'
     APPLICATION_ID = '567067343352427'
     FB_HTTP_ENGINE = 'Liger'
 
-    ANDROID_VERSION = 24
-    ANDROID_RELEASE = '7.0'
-    PHONE_MANUFACTURER = 'samsung'
-    PHONE_DEVICE = 'SM-G930F'
-    PHONE_MODEL = 'herolte'
-    PHONE_DPI = '640dpi'
-    PHONE_RESOLUTION = '1440x2560'
-    PHONE_CHIPSET = 'samsungexynos8890'
-    VERSION_CODE = '138226743'
+    ANDROID_VERSION = 28
+    ANDROID_RELEASE = 9
+    PHONE_MANUFACTURER = 'Xiaomi/Redmi'
+    PHONE_DEVICE = 'Redmi Note 8 Pro'
+    PHONE_MODEL = 'begonia'
+    PHONE_DPI = '440dpi'
+    PHONE_RESOLUTION = '1080x2134'
+    PHONE_CHIPSET = 'mt6785'
+    VERSION_CODE = '209143963'
 
     USER_AGENT_FORMAT = \
         'Instagram {app_version} Android ({android_version:d}/{android_release}; ' \
-        '{dpi}; {resolution}; {brand}; {device}; {model}; {chipset}; en_US; {version_code})'
+        '{dpi}; {resolution}; {brand}; {device}; {model}; {chipset}; tr_TR; {version_code})'
 
     USER_AGENT_EXPRESSION = \
         r'Instagram\s(?P<app_version>[^\s]+)\sAndroid\s\((?P<android_version>[0-9]+)/(?P<android_release>[0-9\.]+);\s' \
