8a4a6d99def34e405fd6e7cc58f9733b8ba8aaa9
Gary Host <ALVAREZ DELGADO Xavier xavier.alvarez@axa-assistance.es>
2020-10-25 20:09:36 +0100

    Fix flake8

--- instagram_private_api/endpoints/upload.py
+++ instagram_private_api/endpoints/upload.py
@@ -388,7 +388,11 @@ class UploadEndpointsMixin(object):
             return ext
         return False
 
-    def upload_image(self, img, size, quality=80, caption='', location=None, disable_comments=False, story=False, **kwargs) -> bool:
+    def upload_image(self, img, size, quality=80, caption='',
+                     location=None,
+                     disable_comments=False,
+                     story=False,
+                     **kwargs) -> bool:
         """
         Upload an image and post it.
         :param img: io.BufferedReader
@@ -467,7 +471,7 @@ class UploadEndpointsMixin(object):
                 )
 
                 res = self.opener.open(req, timeout=self.timeout)
-                post_response = self._read_response(res)
+                self._read_response(res)
 
                 if story:
                     self.configure_to_reel(image_props['upload_id'], size)
